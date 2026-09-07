cb92877b3f036c07002c060cb5d15d171ab3bffe
Shahroof786 <shahroofahmad11@gmail.com>
2026-09-07 15:23:15 +0530

    Basic custom React

---
+++ CustomReact/CustomReact.js
@@ -0,0 +1,28 @@
+function CustomRendrer(reactelement, container){
+    const domelement = document.createElement(reactelement.type)
+    // domelement.innerHTML = reactelement.children
+    // domelement.setAttribute('href', reactelement.props.href)
+    // domelement.setAttribute('target', reactelement.props.target)
+
+    domelement.innerHTML = reactelement.children
+    for(const prop in reactelement.props)
+    {
+        if(prop === 'children') continue;
+        domelement.setAttribute(prop, reactelement.props[prop])
+    }
+
+    container.appendChild(domelement)
+}
+
+const reactelement ={
+    type:'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children:'click here'
+}
+
+const container = document.querySelector('#root')
+
+CustomRendrer(reactelement, container)
