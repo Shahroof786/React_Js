c41b01f103251215d0e2b7b93193097f8bd3a452
Shahroof786 <shahroofahmad11@gmail.com>
2026-09-07 15:35:58 +0530

    create count inc dec using use state and profile card

---
+++ countervite/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import ProfileCard from './components/ProfileCard'
+
+function App() {
+  let [counter,setCounter] = useState(5)
+  const increasevalue = ()=> {
+    counter = counter+1
+    setCounter(counter)
+    console.log(counter)
+  }
+
+  const removeval = ()=> {
+    if(counter != 0)
+    {
+      counter = counter -1
+    }
+    setCounter(counter)
+  }
+
+  let myobj = {
+      myname:'Shaan',
+      myid:7
+    }
+  return (
+    <>
+      <h1>This is my React</h1>
+      <h2>Counter Values: {counter}</h2>
+      <button onClick={increasevalue}>Increase Value</button><br/>
+      <button onClick={removeval}>Decrease Value</button>
+      <ProfileCard myname = "Shaan" btntext="Click here"/>
+      <ProfileCard myname = "Shanu" btntext="Visit me"/>
+    </>
+  )
+}
+
+export default App
